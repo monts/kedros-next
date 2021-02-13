9d260170e669c1dd33a712b6be4bdd481fc5b4c6
Felipe D S Lima <felipe@monts.com.br>
2021-02-13 18:28:22 -0300

    fix bug

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import MontsLogo from '../assets/images/logo.svg'
+import MontsLogo from '../../assets/images/logo.svg'
 import { Container } from './styles'
 
 const Home: React.FC = () => {
